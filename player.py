4fc7bf9379c5614c77a2e791fc4e72213cb54fb5
rtz <r.tellechea.job@gmail.com>
2022-08-16 01:05:09 +0200

    Creada clase Player.

---
+++ player.py
@@ -0,0 +1,19 @@
+class Player():
+	def __init__(self, player_name : int) -> None:
+		if not player_name in (1,2):
+			raise(Exception('O se llama Jugador 1 o se llama Jugador 2.'))
+		self.player_name = player_name
+
+	def __str__(self) -> str:
+		return str(self.player_name)
+
+	def __eq__(self, other) -> bool:
+		return self.player_name == self.player_name
+
+	def __invert__(self):
+		return Player(1) if self.player_name == 2 else Player(2)
+
+if __name__ == '__main__':
+	p = Player(1)
+	q = Player(2)
+	print(~p == q)
