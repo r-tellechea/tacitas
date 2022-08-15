99352871b153e0ad382591ed741885b1c045e636
rtz <r.tellechea.job@gmail.com>
2022-08-16 01:20:32 +0200

    Jugador por defecto 1.

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 class Player():
-	def __init__(self, player_name : int) -> None:
+	def __init__(self, player_name : int = 1) -> None:
 		if not player_name in (1,2):
 			raise(Exception('O se llama Jugador 1 o se llama Jugador 2.'))
 		self.player_name = player_name
@@ -17,3 +17,4 @@ if __name__ == '__main__':
 	p = Player(1)
 	q = Player(2)
 	print(~p == q)
+	print(~ ~ Player() == Player())
